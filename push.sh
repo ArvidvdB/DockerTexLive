4b5421c8c3defd327d606f6c029077297de8bae4
Arvid van den Brink <a.b.vandenbrink@utwente.nl>
2018-09-11 21:42:45 +0200

    Push script and include tag latest

---
+++ push.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -eu
+
+username=( brinkab )
+image=( docker-texlive )
+
+versions=( "$@" )
+if [ ${#versions[@]} -eq 0 ]; then
+	versions=( */ )
+fi
+versions=( "${versions[@]%/}" )
+
+# sort version numbers with highest first
+IFS=$'\n'; versions=( $(echo "${versions[*]}" | sort -rV) ); unset IFS
+
+for version in "${versions[@]}"; do
+    docker push $username/$image:$version
+done
+
+if [ ${#versions[@]} -gt 1 ]; then
+    docker push $username/$image:latest
+fi
